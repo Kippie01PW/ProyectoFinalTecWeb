a9d316444f9120ec730c0ac5b56322e606a87553
BlancaVisca <blancavisca05@gmail.com>
2025-05-24 19:13:20 -0600

    correccion minima

--- app/Views/Formulario.php
+++ app/Views/Formulario.php
@@ -52,7 +52,7 @@ $currentSection = $questions[$page];
 <!DOCTYPE html>
 <html>
 <head>
-  <?php include __DIR__ . '/../Views/layouts/header.php'; ?>
+  <?php include __DIR__ . '/../Views/layouts/header_alumnos.php'; ?>
   <title>Formulario de Intereses</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="/PROYECTOFINALTECWEB/public/assets/js/formulario.js"></script> 

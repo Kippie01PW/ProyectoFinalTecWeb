17daa52a0c067befae901ce474b892ca10b2fe30
Christopher-V36 <christopheraguilar5348@gmail.com>
2025-05-25 02:42:51 -0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -80,7 +80,6 @@ $app->group('/api/alumnos', function ($group) {
     //NUEVO
    // $group->get('/preferencias/formulario', \App\Controllers\PreferenciasAlumnoController::class . ':showFormulario');
    // $group->post('/preferencias/guardar', \App\Controllers\PreferenciasAlumnoController::class . ':guardarPreferencias');
-
 });
 
 // Rutas para Autenticación
@@ -97,13 +96,10 @@ $app->get('/logout', \App\Controllers\AuthController::class . ':logout');
 $app->group('/cursos', function ($group) {
     // Mostrar formulario para crear nuevo curso
     $group->get('/nuevo', CursoController::class . ':showForm');
-    
     // Procesar creación de curso
     $group->post('/guardar', CursoController::class . ':guardarCurso');
-    
     // Listar todos los cursos
     $group->get('/listar', CursoController::class . ':listarCursos');
-    
     // Listar cursos por categoría
     $group->get('/categoria/{id}', CursoController::class . ':listarCursos');
 });

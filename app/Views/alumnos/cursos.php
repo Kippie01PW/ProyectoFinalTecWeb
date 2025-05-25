3be1f888e3573144aceb454a6481af41824045f7
PayoCentral <ph836854@gmail.com>
2025-05-25 17:17:43 -0600

    Update cursos.php

--- app/Views/alumnos/cursos.php
+++ app/Views/alumnos/cursos.php
@@ -54,6 +54,7 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="/ProyectoFinalTecWeb/public/assets/js/MostrarCursos.js"></script>
+    <script src="/PROYECTOFINALTECWEB/public/assets/js/botonID.js"></script>
 
 </body>
 </html>

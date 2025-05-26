1a28929208efcab9631867a9941e785048a6f743
Christopher-V36 <christopheraguilar5348@gmail.com>
2025-05-25 21:36:37 -0600

    Update Base_dashboard.php

--- app/Views/Base_dashboard.php
+++ app/Views/Base_dashboard.php
@@ -1,7 +1,5 @@
 <?php 
 
-include __DIR__ . '/layouts/header_maestro.php';
-
 require_once __DIR__ . '/../Models/Data_Maestro.php';
 require_once __DIR__ . '/../Controllers/MaestroController.php';
 
@@ -38,6 +36,7 @@ $jsHandler = new \App\Controllers\MaestroController($datosPreguntas, $preguntasT
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <?php include __DIR__ . '/layouts/header_maestro.php';?>
     <title>Dashboard Maestros</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
